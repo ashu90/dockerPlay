9e10381fa8fe1452fa57066da5cd9140b04eb485
root <root@vagrant.vm>
2017-07-12 00:15:21 +0000

    2nd docker build image

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def hello():
 	except RedisError:
 		visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-	html = "<h3>Hello this my first docker tutorial {name}!</h3>" \
+	html = "<h3>Hello this my 2nd docker tutorial {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
 	return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
